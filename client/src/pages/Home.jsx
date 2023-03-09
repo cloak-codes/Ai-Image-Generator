9a0b738a1cd31d89433cf0069e3c8e34df7436b5
geo <106122043+cloak-codes@users.noreply.github.com>
2023-03-09 14:24:32 -0800

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
+      const response = await fetch("https://ai-image-generator-7ysq.onrender.com//api/v1/dalle", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
