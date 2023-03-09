29c1c088d60c8973b87a0d1d3f3ea5b7a89e8dd1
geo <106122043+cloak-codes@users.noreply.github.com>
2023-03-09 14:52:21 -0800

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -57,7 +57,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("https://ai-image-generator-7ysq.onrender.com/api/v1/dalle", {
+        const response = await fetch("https://ai-image-generator-7ysq.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
